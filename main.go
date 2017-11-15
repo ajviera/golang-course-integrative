7ffffb5300ec55e403318c949e98a6675d7c15f7
Ariel Jesus Martinez Viera <ariel.jesus.martinez.viera@gmail.com>
2017-11-14 23:41:07 -0300

    add setup for gcp

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	_ "github.com/ajviera/golang-course-integrative/src/integrative"
+	"google.golang.org/appengine"
+)
+
+func main() {
+	appengine.Main()
+}
--- src/integrative/integrative_test.go
+++ src/integrative/integrative_test.go
@@ -1,4 +1,4 @@
-package main
+package integrative
 
 import (
 	"encoding/json"
